1c73caf3b1f80b93baabfb307bd9469b1baec25c
Akila K Gunawardhana <38603664+akilahgm@users.noreply.github.com>
2021-02-15 23:52:28 +0530

    update maincontraller shedular

--- clients/src/main/java/net/corda/samples/server/MainController.java
+++ clients/src/main/java/net/corda/samples/server/MainController.java
@@ -372,7 +372,7 @@ public class MainController {
     }
 
 
-    @Scheduled(initialDelay = 1000, fixedRate = 240000)
+    @Scheduled(initialDelay = 1000, fixedRate = 120000)
     public void run() {
         try {
             BigDecimal valuation = proxy.startTrackedFlowDynamic(GetTokenValuation.class,
